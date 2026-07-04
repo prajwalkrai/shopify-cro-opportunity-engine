4f1222822480dfce4cfb77510ae036dda2e9932c
Prajwal Rai <raiprajwal754@gmail.com>
2026-07-04 12:20:34 +0530

    Connect frontend to Render backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://shopify-cro-opportunity-engine.onrender.com",
 });
 
 export default API;
